ba5d37d99d71572b27691ef851d1511ff7cd3a8b
Francisco <francisco.jimenez.alcantara@gmail.com>
2021-09-27 10:42:59 -0300

    first commint

---
+++ Range/range.rb
@@ -0,0 +1,22 @@
+#Fco.Jimenez
+
+arreglo=[1, 5, 10, -2]
+
+#.include?(value) => devuelve true or false
+puts arreglo.include?(5)
+#.last => devuelve el último objeto en el Range
+puts arreglo.last
+#.max => devuelve el valor máximo en el Range
+puts arreglo.max
+#.min => devuelve el valor mínimo en el Range
+puts arreglo.min
+
+
+#Opcion 2
+miRango= (0..100)
+
+
+puts miRango.include?89
+puts miRango.last
+puts miRango.min
+puts miRango.max 
